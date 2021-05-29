fb4165eb1b25a171cb2b839c2471c8fab1f280fc
luofungyi <teddyluo8233@gmail.com>
2021-05-29 16:02:52 +0800

    add fibonacci.js

--- basic/fibonacci.js
+++ basic/fibonacci.js
@@ -1,11 +1,17 @@
+let counter = 0;
+
 function sum(n) {
+    counter++;
     if(n === 1) {
         return 1;
     }
     return sum(n-1) + n;
 }
 
-sum(10) = sum(9) + 10;
-sum(9) = sum(8) + 9;
+// sum(10) = sum(9) + 10;
+// sum(9) = sum(8) + 9;
 
-function call stack
+// function call stack
+
+console.log(sum(10))
+console.log(counter)
