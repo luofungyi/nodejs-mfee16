83e4d769273f4ce9c0f87202f813bc97b358ae8a
luofungyi <teddyluo8233@gmail.com>
2021-06-05 11:44:38 +0800

    add app-bluebird.js

--- crawler/app-bluebird.js
+++ crawler/app-bluebird.js
@@ -3,63 +3,45 @@ const fs = require("fs");
 const moment = require("moment");
 const Promise = require("bluebird");
 
-// console.log(Promise);
-
-// function readfilePromise() {
-//     return new Promise((resolve, reject) => {
-//         fs.readFile("stock.txt", "utf8", (err, data) => {
-//             if (err) {
-//                 reject(err);
-//             }else {
-//                 resolve(data)
-//               }
-//           })
-//       })
+// 因為用 bluebird 所以不用自己包
+// function readFilePromise() {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile("stock.txt", "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(data);
+//     });
+//   });
 // }
 
+// 方法1: 一個函是一個函式包
 // 用 bluebird 包 callback 版本的 readFile
-const resdFildBlue = Promise.promisify(fs.readFile);
+// const readFile = Promise.promisify(fs.readFile);
 
+// 方法2: 整個 fs 都包起來
+// 把 fs 所有的 function 都包成 promise
+// 但是原本的函式名稱後面會被加上 Async
+const fsBlue = Promise.promisifyAll(fs);
+fsBlue
+  .readFileAsync("stock.txt", "utf-8")
+  .then((stockCode) => {
+    console.log("stockCode:", stockCode);
 
-readfileBlue("stock.txt", "utf-8").then((data) => {
-  return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-    params: {
-      response: JSON,
-      date: moment().format("YYYYMMDD"),
-      stockNo: data
-    }
-  });
-})
-
-
-.then(function (result) {
-    if (result.data.stat === "OK") {
-      console.log(result.data.data);
-      console.log(result.data.title);
+    return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: "json",
+        date: moment().format("YYYYMMDD"),
+        stockNo: stockCode,
+      },
+    });
+  })
+  .then((response) => {
+    if (response.data.stat === "OK") {
+      console.log(response.data.date);
+      console.log(response.data.title);
     }
   })
-
- 
-
-
-// fs.readFile("stock.txt", "utf8", (err, data) => {
-//   if (err) {
-//     return console.error("讀檔錯誤", err);
-//   }
-//   console.log(`讀到的 stock code: ${data}`);
-
-//   axios
-//     .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-//       params: {
-//         response: "json",
-//         date: "20210523",
-//         stockNo: data,
-//       },
-//     })
-//     .then(function (response) {
-//       if (response.data.stat === "OK") {
-//         console.log(response.data.date);
-//         console.log(response.data.title);
-//       }
-//     });
-// });
+  .catch((err) => {
+    console.error(err);
+  });
