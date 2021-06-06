6232b9cc79e914e0e7e1b817287556ffb1ac98fc
luofungyi <teddyluo8233@gmail.com>
2021-06-06 15:33:37 +0800

    modified:   sever.js

--- simple-web/sever.js
+++ simple-web/sever.js
@@ -1,40 +1,38 @@
-// http 是 Node.js 內建的 web sever ， 所以不用安裝
-//https://nodejs.org/dist/latest-v14.x/docs/api/http.html/
-const http = require( "http" );
+// http 是 NodeJS 內建的 web server，所以不用安裝
+// https://nodejs.org/docs/latest-v14.x/api/http.html
+const http = require("http");
 
-
-// createSever(Listener)
-// Listener(request, reponse) 負責處理進來的連線
+// createServer(Listener)
+// Listener(request, response) 負責處理進來的連線
 // request 是請求物件
 // response 是回覆物件
 const server = http.createServer((req, res) => {
- console.log( "是不是有連線阿?" );
-   console.log(req.url);
-   res.statusCode = 200; // 2xx , 3xx , 4xx , 5xx
-   res.setHeader("Content-Type","text/plain;charset=UTF-8");
+  console.log("有連線進來了");
+  console.log(req.url);
 
-   switch(req.res){
-       case "/":
-           res.end("Hi 這是首頁");
-           break;
-           case "/test":
-               res.end("這是測試頁面");
-               break;
-               case "/about":
-                   res.end("這是關於我們");
-                   break;
-                   default:
-                       res.writeHead(404);
-                       res.end("Not Found");
-   }
+  res.statusCode = 200; // 2xx, 3xx, 4xx, 5xx
+  res.setHeader("Content-Type", "text/plain;charset=UTF-8");
+
+  switch (req.url) {
+    case "/":
+      res.end("Hi 這是首頁 我是小賴");
+      break;
+    case "/test":
+      res.end("這是測試頁面");
+      break;
+    case "/about":
+      res.end("這是關於我們");
+      break;
+    default:
+      res.writeHead(404);
+      res.end("Not Found");
+  }
 });
 
-//port
+// port
 server.listen(3000, () => {
-  consolo.log( "我要跑起來了喔，我要收 3000 port")
-
+  console.log("我跑起來了喔，我要收 3000 port");
 });
 
-// PHP 
-//localhost 127.0.0.1
-// Public ID 
+// PHP --> 搭配 web server （apache or nginx)
+// NodeJS 直接開發一個 web server
