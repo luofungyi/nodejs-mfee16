c2703daafaf5165ad775265f2278b5a8f37947d9
luofungyi <teddyluo8233@gmail.com>
2021-06-18 11:07:28 +0800

    add modules

---
+++ modules/car.js
@@ -0,0 +1,31 @@
+// 如果沒有透過 exports 或 module.exports 去暴露資訊的，
+// 其他的就都是 private
+
+// const 其實無法控制物件
+const name = "AA";
+const car = {
+  brand: "Ford",
+  color: "blue",
+};
+
+exports.name = "Ashley";
+
+exports.getColor = function () {
+  return car.color;
+};
+
+exports.setColor = function (color) {
+  if (color == "Yellow" || color == "Red") {
+    car.color = color;
+  }
+  // TODO: 不符合的，不給改
+};
+
+
+//傭有一些控制權，有一些防範
+
+// exports.car = car;
+
+// module.exports = {};
+
+
---
+++ modules/index.js
@@ -0,0 +1,15 @@
+// car.js
+
+// 公有 私有
+
+// 當你暴露給別人時，別人可以寫他、更改
+
+const item = require("./car");
+
+console.log(item);
+
+console.log(item.getColor());
+
+item.setColor("Yellow");
+
+console.log(item.getColor());
