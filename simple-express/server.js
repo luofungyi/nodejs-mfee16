552515db5c963cd3d22f7d1ec48236f005994911
luofungyi <teddyluo8233@gmail.com>
2021-06-20 01:08:39 +0800

    modified

--- simple-express/server.js
+++ simple-express/server.js
@@ -1,53 +1,53 @@
-const connection = require('./utils/db');
-const express = require('express');
+const connection =require("./utils/db")
+const express = require("express");
 let app = express();
 
-app.use(express.static('public'));
 
-app.set('views', 'views');
-app.set('view engine', 'pug');
+app.use(express.static("public"));
 
-app.use(function (req, res, next) {
-    console.log('無用 Middleware');
-    next();
-});
+
+app.set("views","views");
+app.set("view engine","pug");
 
 app.use(function (req, res, next) {
-    let current = new Date();
-    console.log(`有訪客 在 ${current}`);
-    next();
+  console.log("Middleware");
+  next();
 });
 
-let stockRouter = require('./routers/stock');
-app.use('/stock', stockRouter);
+app.use(function(req, res, next){
+  let current = new Date();
+  console.log(`訪問${current}`)
+  next();
+})
 
-let apiRouter = require('./routers/api');
-app.use('/api', apiRouter);
-
-app.get('/', function (req, res) {
-    res.render('index');
+let stockRouter =require("./routers/stock");
+app.use("/stock",stockRouter)
+let apiRouter =require("./routers/api");
+app.use("/api",apiRouter)
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
-app.get('/about', function (req, res, next) {
-    res.render('about');
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
-app.get('/test', function (req, res) {
-    res.send('Test Express');
+app.get("/test", function (req, res) {
+  res.send("Test Express");
 });
 
 app.use(function (req, res, next) {
-    res.status(404);
-    res.render('404');
+  res.status(404);
+  res.render("404");
 });
 
 app.use(function (err, req, res, next) {
-    console.log(err.message);
-    res.status(505);
-    res.send('505');
+  console.log(err.message);
+  res.status(500);
+  res.send("500");
 });
 
-app.listen(3000, async () => {
+app.listen(3000, async()=>{
     await connection.connectAsync();
-    console.log(`已執行 在 port 3000`);
+    console.log(`跑起來了 3000 Port`);
 });
