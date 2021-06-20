10b5872c23d209a50e92eaa73907b1d793ed24a9
luofungyi <teddyluo8233@gmail.com>
2021-06-20 11:26:10 +0800

    modified 驗證

--- simple-express/routers/auth.js
+++ simple-express/routers/auth.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const connection =require("../utils/db");
+
+const bcrypt =require("bcrypt");
 
 const { body, validationResult} = require("express-vaildtor");
 const { validationResult } = require('express-validator');
 
-router.get('/register', (req, res) => {
+router.get('/register', (req, res, next) => {
     res.render('auth/register');
 });
 
@@ -16,15 +19,27 @@ const registerRules = [
     }),
 ];
 
-router.use
-applicationCache.us
 
-router.post('/register',registerRules, (req, res) => {
+router.post('/register',registerRules, async (req, res, next) => {
     //TODO
     // 中間函式(express.urlencoded)設定，
     console.log(req.body);
 
+    // 驗證
     const validationResult = validationResult(req);
+    if(!validationResult.isEmpty()){
+        return next (new Error("註冊資料有問題"));
+    }
+
+    let checkResult = await connection.queryAsync("SELECT * FROM　members WHERE email = ? ",
+    req.body.email
+    ),
+
+    if(checkResult.length > 0) {
+        return next (new Error("已經註冊"));
+    }
+    let result = await connection.queryAsync("INSTERT INTO members (email, passord, name)VALUES (?)",[[req.body.email, await bcrypt.hash(req.body.password, 10), req.body.name]]);
+
     res.send('這是 POST Registe');
 });
 
