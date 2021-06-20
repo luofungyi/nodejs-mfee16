8b5ace4298a3aadeb5e25818899d371bb3593615
luofungyi <teddyluo8233@gmail.com>
2021-06-20 15:29:04 +0800

    modified

--- simple-express/routers/auth.js
+++ simple-express/routers/auth.js
@@ -1,5 +1,5 @@
-const router = express.Router();
 const express = require('express');
+const router = express.Router();
 const connection = require('../utils/db');
 const bcrypt = require('bcrypt');
 const path = require('path');
@@ -8,22 +8,35 @@ const { body, validationResult } = require('express-validator');
 
 // 檔案放在
 const storage = multer.diskStorage({
-    destination: function (req, res, cd) {
+    destination: function (req, file, cb) {
         cd(null, path.join(__dirname, '../', 'public', 'uploads'));
     },
-    // firename
+    filename: function (req, file, cb) {
+        // 拿到附檔名
+        const ext = file.originalname.split('.').pop();
+        // 自己想要的檔案名
+        cb(null, `${file.fieldname}-${Date.now()}.${ext}`);
+    },
 });
 // multer 上傳工具
 const uploader = multer({
     storage: storage,
-    filerfilter: function (req, res, cd) {
-        if (!file.mimetype == '') {
-            return cd (new Error("file type err "), false);
+    filefilter: function (req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('是不合格的副檔名'));
         }
-        if(!file.originalname)
+        // 檔案ＯＫ, 接受這個檔案
+        cb(null, true);
+    },
+    limits: {
+        // 檔案的上限 1M
+        fileSize: 1024 * 1024,
     },
 });
 
+// --
+
+// --
 router.get('/register', (req, res, next) => {
     res.render('auth/register');
 });
@@ -55,6 +68,8 @@ router.post('/register', registerRules, async (req, res, next) => {
     if (checkResult.length > 0) {
         return next(new Error('已經註冊'));
     }
+    let filepath = req.file ? '/uploads/' + req.file.filename : null;
+
     let result = await connection.queryAsync(
         'INSTERT INTO members (email, passord, name)VALUES (?)',
         [
@@ -66,11 +81,55 @@ router.post('/register', registerRules, async (req, res, next) => {
         ]
     );
 
-    res.send('這是 POST Registe');
+    res.send('註冊成功');
 });
 
 router.get('/login', (req, res) => {
     res.render('auth/rlogin');
+});
+
+const loginRules = [
+    body('email').isEmail().withMessage('請輸入正確格式'),
+    body('password').isLength({ min: 6 }),
+];
+
+router.post('/login', loginRules, async (req, res) => {
+    console.log(req.body);
+
+    const validationResult = validationResult(req);
+    if (!validationResult.isEmpty()) {
+        return next(new Error('登入資料有問題'));
+    }
+
+    // 檢查 email
+    let checkResult = await connection.queryAsync(
+        'SELECT * FROM　members WHERE email = ? ',
+        req.body.email
+    );
+
+    if (checkResult.length === 0) {
+        return next(new Error('查無帳號'));
+    }
+
+    member = member[0];
+
+    // 比對密碼
+
+    let result = await bcrypt.compare(req.body.password, member.password);
+    if (result) {
+        req.session.member = {
+            email: member.email,
+            name: member.name,
+            photo: member.photo,
+        };
+        res.redirect(303, '/login');
+        // res.send('登入成功');
+    } else {
+        req.session.isLogin = true;
+        res.send('登入失敗');
+    }
+
+    res.send('登入結果 ?');
 });
 
 module.exports = router;
--- simple-express/server.js
+++ simple-express/server.js
@@ -1,61 +1,81 @@
-const connection =require("./utils/db")
-const express = require("express");
+const connection = require('./utils/db');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
 let app = express();
 
+// 拿到 post 資料， 若 npm i boby-parser
+app.use(express.urlencoded({ extended: false }));
+// 前端送 json
+app.use(express.json());
+// 拿到 cookie
+app.use(cookieParser());
+// 拿到 session
+app.use(
+    expressSession({
+        secrect: process.env.SESSION_SECRET,
+        saveUninitialized: false,
+    })
+);
 
-// 解讀 post 資料， 若 npm i boby-parser
-app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
 
-app.use(express.static("public"));
-
-
-app.set("views","views");
-app.set("view engine","pug");
+app.set('views', 'views');
+app.set('view engine', 'pug');
 
 app.use(function (req, res, next) {
-  console.log("Middleware");
-  next();
+    // req.session 設定給 res.locals < 透過這個方式把資料給views
+    res.locals.member = req.session.member;
+    next();
 });
 
-app.use(function(req, res, next){
-  let current = new Date();
-  console.log(`訪問${current}`)
-  next();
-})
+app.use(function (req, res, next) {
+    console.log('Middleware');
+    next();
+});
 
-let stockRouter =require("./routers/stock");
-app.use("/stock",stockRouter)
-let apiRouter =require("./routers/api");
-app.use("/api",apiRouter)
-let authRouter =require("./routers/auth");
-app.use("/auth",authRouter)
+app.use(function (req, res, next) {
+    let current = new Date();
+    console.log(`訪問${current}`);
+    next();
+});
+
+let stockRouter = require('./routers/stock');
+app.use('/stock', stockRouter);
+let apiRouter = require('./routers/api');
+app.use('/api', apiRouter);
+let authRouter = require('./routers/auth');
+app.use('/auth', authRouter);
 // /auth 網址自定義
 
+app.get('/', function (req, res) {
+    console.log('這是首頁');
 
-app.get("/", function (req, res) {
-  res.render("index");
+    res.cookie('lang', 'zh-TW');
+
+    res.render('index');
 });
 
-app.get("/about", function (req, res) {
-  res.render("about");
+app.get('/about', function (req, res) {
+    res.render('about');
 });
 
-app.get("/test", function (req, res) {
-  res.send("Test Express");
+app.get('/test', function (req, res) {
+    res.send('Test Express');
 });
 
 app.use(function (req, res, next) {
-  res.status(404);
-  res.render("404");
+    res.status(404);
+    res.render('404');
 });
 
 app.use(function (err, req, res, next) {
-  console.log(err.message);
-  res.status(500);
-  res.send("500");
+    console.log(err.message);
+    res.status(500);
+    res.send('500');
 });
 
-app.listen(3000, async()=>{
+app.listen(3000, async () => {
     await connection.connectAsync();
     console.log(`跑起來了 3000 Port`);
 });
